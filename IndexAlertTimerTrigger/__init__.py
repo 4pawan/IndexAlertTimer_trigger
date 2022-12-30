00ba138b1e39edd0b53dcf73834e13ac629d22f3
Pawan Singh <pwn.singh14@gmail.com>
2022-12-30 16:24:13 +0530

    init commit

---
+++ IndexAlertTimerTrigger/__init__.py
@@ -0,0 +1,46 @@
+import datetime
+import logging
+import requests
+from nsetools import Nse
+import azure.functions as func
+nse= Nse()
+TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+chat_id = -100XXXXXXX
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    nifty = nse.get_index_quote('nifty 50')
+    banknifty = nse.get_index_quote('nifty bank')    
+    lastPrice = nifty['lastPrice']
+    change = nifty['change']      
+
+    if change > 100:   
+       message = f"Nifty>100 change:{change} at price:{lastPrice}"       
+       url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+       requests.get(url).json()
+
+    if change < -100:   
+       message = f"Nifty<-100 change:{change} at price:{lastPrice}"       
+       url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+       requests.get(url).json()
+         
+    bankNiftyLastPrice = banknifty['lastPrice']
+    bankNiftyLastChange = banknifty['change']      
+
+    if bankNiftyLastChange > 500:   
+       message3 = f"BankNifty>500 change:{bankNiftyLastChange} at price:{bankNiftyLastPrice}"       
+       url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message3}"
+       requests.get(url).json()
+
+    if bankNiftyLastChange < -500:   
+       message4 = f"BankNifty<-500 change:{bankNiftyLastChange} at price:{bankNiftyLastPrice}"       
+       url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message4}"
+       requests.get(url).json()
+    
---
+++ README.md
@@ -0,0 +1,4 @@
+# IndexAlertTimerTrigger
+
+
+https://medium.com/codex/using-python-to-send-telegram-messages-in-3-simple-steps-419a8b5e5e2
