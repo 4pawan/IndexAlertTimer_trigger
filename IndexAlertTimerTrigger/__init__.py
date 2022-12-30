46b822c94951e3638aff916277b5df7d6a585692
Pawan Singh <pwn.singh14@gmail.com>
2022-12-30 23:41:42 +0530

    init commit

--- IndexAlertTimerTrigger/__init__.py
+++ IndexAlertTimerTrigger/__init__.py
@@ -3,13 +3,14 @@ import logging
 import requests
 from nsetools import Nse
 import azure.functions as func
-nse= Nse()
-TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-chat_id = -100XXXXXXX
+
+nse = Nse()
+TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+chat_id = -100xxxxxxxx
+
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
+    utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
@@ -17,37 +18,34 @@ def main(mytimer: func.TimerRequest) -> None:
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
     nifty = nse.get_index_quote('nifty 50')
-    banknifty = nse.get_index_quote('nifty bank')    
-    hdfclife = nse.get_quote('hdfclife')    
-    lastPrice = nifty['lastPrice']
-    change = nifty['change']      
-   
-    message_welcome = f"Welcome:{nifty['change']} :{hdfclife['change']}"       
+    banknifty = nse.get_index_quote('nifty bank')
+    hdfclife = nse.get_quote('hdfclife')
+    lastprice = float(nifty['lastPrice'])
+    change = float(nifty['change'])
+
+    message_welcome = f"Welcome:{nifty['change']} :{hdfclife['change']}"
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message_welcome}"
-    requests.get(url).json()
+    requests.get(url)
 
+    if change > 100:
+        message = f"Nifty>100 change:{change} at price:{lastprice}"
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+        requests.get(url)
 
+    if change < -100:
+        message = f"Nifty<-100 change:{change} at price:{lastprice}"
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+        requests.get(url)
 
-    if change > 100:   
-       message = f"Nifty>100 change:{change} at price:{lastPrice}"       
-       url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-       requests.get(url).json()
+    bankniftylastprice = float(banknifty['lastPrice'])
+    bankniftylastchange = float(banknifty['change'])
 
-    if change < -100:   
-       message = f"Nifty<-100 change:{change} at price:{lastPrice}"       
-       url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-       requests.get(url).json()
-         
-    bankNiftyLastPrice = banknifty['lastPrice']
-    bankNiftyLastChange = banknifty['change']      
+    if bankniftylastchange > 500:
+        message3 = f"BankNifty>500 change:{bankniftylastchange} at price:{bankniftylastprice}"
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message3}"
+        requests.get(url)
 
-    if bankNiftyLastChange > 500:   
-       message3 = f"BankNifty>500 change:{bankNiftyLastChange} at price:{bankNiftyLastPrice}"       
-       url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message3}"
-       requests.get(url).json()
-
-    if bankNiftyLastChange < -500:   
-       message4 = f"BankNifty<-500 change:{bankNiftyLastChange} at price:{bankNiftyLastPrice}"       
-       url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message4}"
-       requests.get(url).json()
-    
+    if bankniftylastchange < -500:
+        message4 = f"BankNifty<-500 change:{bankniftylastchange} at price:{bankniftylastprice}"
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message4}"
+        requests.get(url)
